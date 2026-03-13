54594b5d38eee6d50ae01087ec081e19be0b98ec
Guess Mode <guessmode206@gmail.com>
2026-03-13 15:03:16 +0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 
 const app = express();
 
-/* CORS configuration */
 const corsOptions = {
   origin: ["https://hyw-rma.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -13,13 +12,14 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.options("/", cors(corsOptions)); // FIXED
-
+app.options("/", cors(corsOptions));
 app.use(express.json());
-app.use("/api/hyw", require("./routes/hywRoutes"));
+
 app.get("/", (req, res) => {
   res.send("HYW RMA API is running");
 });
+app.use("/api/hyw", require("./routes/hywRoutes"));
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
